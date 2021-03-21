64a21a6a1807b2eff2fffd7f7a43f0014199608d
tustran9499 <minhtu.tran.09041999@gmail.com>
2021-03-21 21:02:27 +0700

    [b] Fix account route

--- src/themes/account/layout/index.tsx
+++ src/themes/account/layout/index.tsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 import PageNotFound from "../../../modules/no-route";
 import { accountsRoutes } from "../routers/routes";
 
 export default function AccountLayout() {
   return (
-    <Switch>
-      {accountsRoutes
-        .filter((item: any) => !item.isLayout)
-        .map((item: any, index: number) => (
-          <Route
-            key={index}
-            path={item.path}
-            component={item.component}
-            exact={item.exact}
-            render={item.component}
-          />
-        ))}
-      <Route component={PageNotFound} />
-    </Switch>
+    <Suspense fallback={<div>Loading... </div>}>
+      <Switch>
+        {accountsRoutes
+          .filter((item: any) => !item.isLayout)
+          .map((item: any) => (
+            <Route
+              path={item.path}
+              component={item.component}
+              exact={item.exact}
+              render={item.component}
+            />
+          ))}
+        {/* <Route component={PageNotFound} /> */}
+      </Switch>
+    </Suspense>
   );
 }
--- src/themes/account/routers/routes.tsx
+++ src/themes/account/routers/routes.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export const accountsRoutes = [
   {
-    path: '/',
+    path: '/account',
     name: 'home',
     title: 'Home Page',
     exact: true,
     permission: '',
     component: React.lazy(() => import('../pages/Home')),
     isLayout: false,
-    isGuarded: true,
+    isGuarded: false,
   },
 ];
